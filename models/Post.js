6679751dc9e5a9922af770a8007bc1207bce5f6a
nndeto <86444679+nndeto@users.noreply.github.com>
2021-09-05 23:16:28 -0400

    added validators in Post schema but can't figure out how to test

--- models/Post.js
+++ models/Post.js
@@ -2,8 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    title: {type: String, minLength: 1, maxLength: 200},
-    content: {type: String, required: true}
+    title: {
+        type: String, 
+        minLength: 1, 
+        maxLength: 200,
+        validate: {
+            validator: function(v) {
+                return v.length > 1 || v.length < 200;
+            },
+            message: `Title must be between 1 and 200 characters long.`  
+        }
+    },
+    content: {
+        type: String, 
+        required: true,
+        validate: {
+            validator: function(v) {
+                return v.length > 1
+            }, 
+            message: `Content cannot be emptpy.`
+        }
+    }
     //to be done after mvp is met
     //date: type: Date,
     //picture: type: string //cover photo for the post,
