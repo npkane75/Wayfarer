eb934c0438ba2528166c60d4bee4ef7f53034ad1
nndeto <86444679+nndeto@users.noreply.github.com>
2021-09-05 22:50:47 -0400

    added requirement to post content schema and min/max length to title in post schema

--- models/Post.js
+++ models/Post.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    title: String,
-    content: String,
+    title: {type: String, minLength: 1, maxLength: 200},
+    content: {type: String, required: true}
     //to be done after mvp is met
     //date: type: Date,
     //picture: type: string //cover photo for the post,
